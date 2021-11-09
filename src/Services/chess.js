96fa4950ff319b53c9d6d077746ac9b0e8b8d6bd
Juan Cruz Alvarez <juancruzalvarezgonzalez@gmail.com>
2021-11-08 22:00:37 -0300

    k

--- src/Services/chess.js
+++ src/Services/chess.js
@@ -26,44 +26,55 @@ export const isMoveValid = (position, moveStart, moveEnd) =>{
       piece: position.board[moveStart],                                 //piece to move
    };
    start.pieceColor = start.piece == start.piece.toLowerCase() ? 'b': 'w';
+   start.pieceColor = start.piece ? start.pieceColor : '';
    let end = {                                                          //end square of the move
       x: moveEnd%8,
       y: Math.floor(moveEnd/8),
       piece: position.board[moveEnd],                                  //piece at the square we want to move to
    };
    end.pieceColor = end.piece == end.piece.toLowerCase() ? 'b': 'w';
+   end.pieceColor = end.piece ? end.pieceColor : '';
 
-   if(start.pieceColor != position.move){    //canot move oponent pieces
+   let yMovement = Math.abs(end.y-start.y);
+   let xMovement = Math.abs(end.x-start.x);
+
+   if(start.pieceColor != position.move){    //cannot move oponent's pieces.
       return false;
    }
+   if(start.pieceColor == end.pieceColor){   //cannot capture your own pieces.   FIX_FOR_CASTLE
+      alert('help');
+      return false;
+   }
+
    switch(start.piece.toLowerCase()){
+
       case '':{
          return false;     
       }
+
       case 'p':{
-         let yMovement = Math.abs(end.y-start.y);
-         let xMovement = Math.abs(end.x-start.x);
-         alert(xMovement+" "+ yMovement);
-         if(xMovement = 1){
+         
+         if(xMovement == 1){
             //then if the move is valid it can only be a capture.
             if(yMovement!=1){
                return false;
             }else{
-               if(end.piece =='' ||
-               (start.pieceColor == end.pieceColor && moveEnd != position.enpassantSquare)){
+               if(end.piece ==''&& moveEnd != position.enpassantSquare){
                   
                   return false;
                }
             }
-         }else if(xMovement = 0){
-            if(yMovement = 1){
+         }else if(xMovement == 0){
+            if(yMovement == 1){
+               
                if(end.piece !=''){  //can only move to empty square.
                   return false;
                }
-            }else if(yMovement = 2){
+            }else if(yMovement == 2){
                //can only move two squares if its at starting position.
+               
                if(start.pieceColor == 'w'){
-                  if(start.y != 7){
+                  if(start.y != 6){
                      return false;
                   }
                }else{
@@ -78,7 +89,36 @@ export const isMoveValid = (position, moveStart, moveEnd) =>{
             //pawns cannot move more than one square horizontally.
             return false;
          }
+         break;
       }
+
+      case 'n':{
+         if(xMovement == 2){
+            if(yMovement != 1){
+               return false;
+            }
+         }else if(xMovement == 1){
+            if(yMovement !=2){
+               return false;
+            }
+         }else{
+            return false;
+         }
+         break;
+      }
+
+      case 'b':{
+         if(xMovement!=yMovement){
+            return false;
+         }else{
+            let stepX = Math.sign(end.x-start.x);
+            let stepY = Math.sign(end.y-start.y);
+            for(let i= 0; i<xMovement-1;i++){ // we do xMovement-1 becouse the target square is already checked at the top, it could also be yMovement-1
+               let squareInPath = start.y+i*stepY*8 + (start.x+i*stepX);
+            }
+         }
+      }
+
    }
    return true;
 }
