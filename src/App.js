5723da110d39964c8b0346808e5ade31c553a206
Juan Cruz Alvarez <juancruzalvarezgonzalez@gmail.com>
2021-11-20 02:13:20 -0300

    recursive tree view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import {ChessBoard} from './Components/chessboard'
 import {Main} from './Components/main'
+
 function App() {
   return (
     <div className="App">
--- src/Components/main.js
+++ src/Components/main.js
@@ -3,13 +3,34 @@ import Style from './styles.css'
 import { startPosition } from '../Services/chess';
 import {Board} from './Board/Board'
 import {GameMovesDisplay}from './GameMovesDisplay/GameMovesDisplay'
+import {Node} from  '../Services/moveTree'
 
 export class Main extends React.Component{
    constructor(props){
       super(props);
+      let e4 = new Node('e4');
+      let e5 = new Node('e5');
+      let nf3 = new Node('nf3');
+      let nc3 = new Node('nc3');
+      let nc6 = new Node('nc6');
+      let nf6 = new Node('nf6');
+      let nxe5 = new Node('nxe5');
+      
+      nc3.addChildren(nc6);
+      nf6.addChildren(nxe5);
+      nf3.addChildren(nf6);
+      e5.addChildren(nc3);
+      e5.addChildren(nf3);
+      e4.addChildren(e5);
+      
+     
+      
+      
+
+
       this.state = {
          position: startPosition,
-         moves: ['e4', 'e5', 'Nf6','Nc6', 'e4', 'e5', 'Nf6','Nc6', 'e4', 'e5', 'Nf6','Nc6', 'e4', 'e5', 'Nf6','Nc6','e4', 'e5', 'Nf6','Nc6', 'e4', 'e5', 'Nf6','Nc6', 'e4', 'e5', 'Nf6','Nc6', 'e4', 'e5', 'Nf6','Nc6','e4', 'e5', 'Nf6','Nc6', 'e4', 'e5', 'Nf6','Nc6', 'e4', 'e5', 'Nf6','Nc6', 'e4', 'e5', 'Nf6','Nc6']
+         moves: e4
       };
       
    }
