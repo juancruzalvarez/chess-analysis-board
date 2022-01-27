b96f1312fe3d1d8e858c14e5917104ebbea94a12
Juan Cruz Alvarez <juancruzalvarezgonzalez@gmail.com>
2022-01-27 17:43:15 -0300

    f

--- src/Components/main.js
+++ src/Components/main.js
@@ -1,10 +1,11 @@
-import {useState, useRef, useEffect} from 'react'
+
+import {useState, useRef} from 'react';
 //eslint-disable-next-line
-import Style from './styles.css'
-import { startPosition, isMoveValid, performMove, getPositionFromFEN, squareNotationToIndex, indexToSquareNotation, getFENfromPosition, moveType, colors, longToShortAlgebraicNotation} from '../Services/chess';
-import {Board} from './Board/Board'
-import {GameMovesDisplay}from './GameMovesDisplay/GameMovesDisplay'
-import {Node, searchNode, insertNode} from  '../Services/moveTree'
+import Style from './styles.css';
+import { startPosition, isMoveValid, performMove, squareNotationToIndex, getFENfromPosition, longToShortAlgebraicNotation} from '../Services/chess';
+import {Board} from './Board/Board';
+import {GameMovesDisplay}from './GameMovesDisplay/GameMovesDisplay';
+import {Node, insertNode} from  '../Services/moveTree';
 import { EngineEvaluation } from './EngineEvaluation/EngineEvaluation';
 import {cloneDeep} from 'lodash';
 
@@ -28,7 +29,7 @@ export const Analysis = (props) =>{
       let x = Math.floor(8 * ( (clientX-boundingRect.left)/boundingRect.width ) );
       let y = Math.floor(8 * ( (clientY-boundingRect.top)/boundingRect.height ) );
       return y*8 + x;
-   }
+   };
 
    const restartEvaluation = (position) =>{
       if(stockfishRef.current){
@@ -73,14 +74,14 @@ export const Analysis = (props) =>{
       stockfishRef.current.postMessage('ucinewgame');
       stockfishRef.current.postMessage('position fen '+getFENfromPosition(position));
       stockfishRef.current.postMessage('go depth 22');
-   }
+   };
    
    const handleOnMouseDown = (e) =>{
       let index = getBoardSquare(e.clientX, e.clientY);
       if(position.board[index]){
          setSelectedSquare(index);
       }
-   }
+   };
 
    const handleOnMouseUp = (e) =>{
       let index = getBoardSquare(e.clientX, e.clientY);
@@ -97,17 +98,17 @@ export const Analysis = (props) =>{
          restartEvaluation(newPosition);
       }
       setSelectedSquare(null);
-   }
+   };
 
    const handleOnMouseMove = (e)=>{
       setMousePosition({x:e.clientX, y:e.clientY});
-   }
+   };
 
    const handleOnClickNode = (clickedNodeId, clickedNodePosition) =>{
       setCurrentNodeID(clickedNodeId);
       setPosition(clickedNodePosition);
       restartEvaluation(clickedNodePosition);
-   }
+   };
 
    const handleEnginetoggle = ()=>{
       if(engineOn){ //if it was on kill thread and reset.
