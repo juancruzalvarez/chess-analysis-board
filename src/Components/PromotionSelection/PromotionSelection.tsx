d6a6961f4b37ec597a87c9ed276ff823de6baaf8
Juan Cruz Alvarez <juancruzalvarezgonzalez@gmail.com>
2022-02-06 15:36:50 -0300

    promotion

---
+++ src/Components/PromotionSelection/PromotionSelection.tsx
@@ -0,0 +1,10 @@
+export const PromotionSelection = (props) =>{
+   return (
+      <div className = 'PromotionSelectionContainer'>
+         <div className = 'PromotionSelectionImage'>N</div>
+         <div className = 'PromotionSelectionImage'>B</div>
+         <div className = 'PromotionSelectionImage'>R</div>
+         <div className = 'PromotionSelectionImage'>Q</div>
+      </div>
+   );
+}
